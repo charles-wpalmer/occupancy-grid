3bada1c82771de4ab08857aa2589c124ee801040
chp <chp@gloversure.co.uk>
2017-03-23 00:00:36 +0000

    Created and started AStar class

---
+++ src/AI/AStar.java
@@ -0,0 +1,29 @@
+package AI;
+
+import robot.Grid;
+
+/**
+ * Class to handle A* searching.
+ * Created by charles on 22/03/17.
+ */
+public class AStar implements IAStar{
+    @Override
+    public void expandAll(Grid grid, int depth) {
+
+    }
+
+    @Override
+    public void finalise() {
+
+    }
+
+    @Override
+    public void playSolution() {
+
+    }
+
+    @Override
+    public int calculateCost() {
+        return 0;
+    }
+}
